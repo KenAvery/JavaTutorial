7e29f6bcff9aae6ad7f24303c80d0ade04a7700f
Ken Avery <ken.avery@hp.com>
2014-06-21 16:18:37 -0500

    Added objects package with a couple demos
    Change-Id: I8d478ab8be08174cdb1a5729814c06e7c955c822

---
+++ tutorial/src/main/java/org/ken/avery/objects/MountainBike.java
@@ -0,0 +1,53 @@
+package org.ken.avery.objects;
+
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public class MountainBike extends Bicycle
+{
+
+    // the MountainBike subclass has one field
+    public int seatHeight;
+
+    // the MountainBike subclass has one constructor
+    public MountainBike(final int startHeight, final int startCadence,
+            final int startSpeed, final int startGear)
+    {
+        super(startCadence, startSpeed, startGear);
+        seatHeight = startHeight;
+    }
+
+    // the MountainBike subclass has one method
+    public void setHeight(final int newValue)
+    {
+        seatHeight = newValue;
+
+    }
+}
