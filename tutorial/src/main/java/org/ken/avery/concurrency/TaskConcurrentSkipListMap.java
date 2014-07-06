bedee4a41c505279e880cb6a195ea4affc852de0
Ken Avery <ken.avery@hp.com>
2014-07-06 06:59:05 -0500

    Added Concurrent Skip List Example
    Change-Id: I3ff84cc21f0068f477b387e58460d972cfb07ba8

---
+++ tutorial/src/main/java/org/ken/avery/concurrency/TaskConcurrentSkipListMap.java
@@ -0,0 +1,39 @@
+package org.ken.avery.concurrency;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.Semaphore;
+
+public class TaskConcurrentSkipListMap implements Runnable
+{
+    private Integer key = null;
+    private final String name;
+    private static Semaphore semaphore = null;
+    private static ConcurrentSkipListMap<Integer, String> concurrentSkipListMap = null;
+
+    TaskConcurrentSkipListMap(
+            final Semaphore semaphore,
+            final Integer key,
+            final String name,
+            final ConcurrentSkipListMap<Integer, String> concurrentNavigableMap)
+    {
+        this.key = key;
+        this.name = name;
+        TaskConcurrentSkipListMap.semaphore = semaphore;
+        TaskConcurrentSkipListMap.concurrentSkipListMap = concurrentNavigableMap;
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            semaphore.acquire();
+            concurrentSkipListMap.put(key, name);
+            semaphore.release();
+        }
+        catch (final InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
