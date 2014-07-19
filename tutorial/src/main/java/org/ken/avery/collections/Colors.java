158bda3a0b55bc8fc6a61da60bd24c6637013409
ken.avery <ken.avery@hp.com>
2014-07-19 12:28:36 -0500

    Added fun with deque (deck)
    Change-Id: I762a3199d926025396fcfb1a931b5985725dc787

---
+++ tutorial/src/main/java/org/ken/avery/collections/Colors.java
@@ -0,0 +1,44 @@
+package org.ken.avery.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Colors
+{
+    public static final String RED = "Red";
+    public static final String AERO = "Aero";
+    public static final String ALMOND = "Almond";
+    public static final String AMBER = "Amber";
+    public static final String BEIGE = "Beige";
+    public static final String WHITE = "White";
+    public static final String BLUE = "Blue";
+    public static final String CHARCOAL = "Charcoal";
+    public static final String PINK = "Pink";
+    public static final String FALLOW = "Fallow";
+    public static final String GREEN = "Green";
+    public static final String BLACK = "Black";
+    public static final String GRAY = "Gray";
+    public static final String AZURE = "Azure";
+
+    public static Map<Integer, String> colors()
+    {
+        final Map<Integer, String> colors = new HashMap<Integer, String>();
+
+        colors.put(1, RED);
+        colors.put(2, AERO);
+        colors.put(3, ALMOND);
+        colors.put(4, AMBER);
+        colors.put(5, BEIGE);
+        colors.put(6, WHITE);
+        colors.put(7, BLUE);
+        colors.put(8, CHARCOAL);
+        colors.put(9, PINK);
+        colors.put(10, FALLOW);
+        colors.put(11, GREEN);
+        colors.put(12, BLACK);
+        colors.put(13, GRAY);
+        colors.put(14, AZURE);
+
+        return colors;
+    }
+}
---
+++ tutorial/src/main/java/org/ken/avery/collections/FunWithDeque.java
@@ -0,0 +1,132 @@
+package org.ken.avery.collections;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class FunWithDeque
+{
+    private static Deque<String> colorDeque = new ArrayDeque<String>(Arrays.asList(
+            "Aero",
+            "Almond",
+            "Amber",
+            "Beige",
+            "White",
+            "Blue",
+            "Red",
+            "Charcoal",
+            "Pink",
+            "Fallow"));
+
+    private static List<String> colorList = new LinkedList<String>(Arrays.asList(
+            "Red",
+            "Green",
+            "Black",
+            "White",
+            "Black",
+            "Gray",
+            "Red",
+            "White",
+            "Blue"));
+
+    public static void main(final String[] args)
+    {
+        mergeLists();
+        addColorsFirst();
+        addColorsLast();
+        removeColors(colorList);
+        reverseOrder();
+    }
+
+    /**
+     * Merge colorDeque and colorList
+     */
+    public static void mergeLists()
+    {
+        Collections.addAll(colorDeque, colorList.toArray(new String[0]));
+        System.out.println("Color Deque: " + colorDeque);
+    }
+
+    /**
+     * Remove all from colorDeque and add colors from Colors class FIFO
+     */
+    public static void addColorsFirst()
+    {
+        colorDeque.removeAll(colorDeque);
+        final Iterator<Entry<Integer, String>> iterator = Colors.colors().entrySet().iterator();
+        while (iterator.hasNext())
+        {
+            final Entry<Integer, String> color = iterator.next();
+            if (!colorDeque.contains(color.getValue()))
+            {
+                colorDeque.addFirst(color.getValue());
+            }
+        }
+        System.out.println("Color Deque: " + colorDeque);
+    }
+
+    /**
+     * Remove all from colorDeque and add colors from Colors class LIFO
+     */
+    public static void addColorsLast()
+    {
+
+        colorDeque.removeAll(colorDeque);
+        final Iterator<Entry<Integer, String>> iterator = Colors.colors().entrySet().iterator();
+        while (iterator.hasNext())
+        {
+            final Entry<Integer, String> color = iterator.next();
+            if (!colorDeque.contains(color.getValue()))
+            {
+                colorDeque.addLast(color.getValue());
+            }
+        }
+        System.out.println("Color Deque: " + colorDeque);
+    }
+
+    /**
+     * Remove the colors in colorList from colorDeque
+     * 
+     * @param colorList
+     */
+    private static void removeColors(final List<String> colorList)
+    {
+        for (int i = 0; i < colorDeque.size(); i++)
+        {
+            final Iterator<String> iterator = colorList.iterator();
+            while (iterator.hasNext())
+            {
+                final String color = iterator.next();
+                if (colorDeque.contains(color))
+                {
+                    colorDeque.remove(color);
+                }
+
+            }
+        }
+        System.out.println("Color Deque: " + colorDeque);
+    }
+
+    /**
+     * Reverse the order of colorDeque
+     */
+    private static void reverseOrder()
+    {
+        final Deque<String> deque = new ArrayDeque<String>();
+        for (int i = 0; i < colorDeque.size(); i++)
+        {
+            final String firstColor = colorDeque.peekFirst();
+            colorDeque.removeFirst();
+            colorDeque.addLast(firstColor);
+            deque.addFirst(firstColor);
+        }
+        colorDeque.removeAll(colorDeque);
+        colorDeque.addAll(deque);
+        System.out.println("Color Deque: " + colorDeque);
+    }
+}
