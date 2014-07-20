7de775948c4a744a95dfe593c8b2f607ca12b1a5
ken.avery <ken.avery@hp.com>
2014-07-20 11:34:10 -0500

    Spruced up the Fun with sub sets printouts
    Change-Id: I83f33f38aeca33262d3767b557a7fb3aae3e02ec

--- tutorial/src/main/java/org/ken/avery/collections/FunWithSortedSets.java
+++ tutorial/src/main/java/org/ken/avery/collections/FunWithSortedSets.java
@@ -10,13 +10,18 @@ public class FunWithSortedSets
     {
         final SortedSet<String> raceHorseSet = new TreeSet<String>(RaceHorses.raceHorses().keySet());
 
-        System.out.println(raceHorseSet);
-        System.out.println(raceHorseSet.headSet("Handsomchamp"));
-        System.out.println(raceHorseSet.headSet("Handsomchamp\0"));
-        System.out.println(raceHorseSet.tailSet("Handsomchamp"));
-        System.out.println(raceHorseSet.tailSet("Handsomchamp\0"));
-        System.out.println(raceHorseSet.subSet("Handsomchamp\0", "Ormonde"));
-        System.out.println(raceHorseSet.subSet("Handsomchamp", "Ormonde\0"));
-        System.out.println(raceHorseSet.subSet("Handsomchamp", "Ormonde\0").size());
+        System.out.println("Race Horse Set: " + raceHorseSet);
+        System.out.println("Head Set starting at \"Handsomechamp\"       : " + raceHorseSet.headSet("Handsomchamp"));
+        System.out.println("Head Set starting at \"Handsomechamp\\0\"     : " + raceHorseSet.headSet("Handsomchamp\0"));
+        System.out.println("Tail Set starting at \"Ormonde\"             : " + raceHorseSet.tailSet("Ormonde"));
+        System.out.println("Tail Set starting at \"Ormonde\\0\"           : " + raceHorseSet.tailSet("Ormonde\0"));
+        System.out.println("Sub Set \"Handsomechamp\\0\", \"Ormonde\"       : " + raceHorseSet.subSet("Handsomchamp\0", "Ormonde"));
+        System.out.println("Sub Set \"Handsomechamp\", \"Ormonde\\0\"       : " + raceHorseSet.subSet("Handsomchamp", "Ormonde\0"));
+        System.out.println("Sub Set Size \"Handsomechamp\", \"Ormonde\\0\"  : "
+                + raceHorseSet.subSet("Handsomchamp", "Ormonde\0").size());
+        System.out.println("Sub Set First \"Handsomechamp\", \"Ormaonde\"  : "
+                + raceHorseSet.subSet("Handsomchamp", "Ormonde").first());
+        System.out.println("Sub Set Last \"Handsomechamp\", \"Ormonde\"    : "
+                + raceHorseSet.subSet("Handsomchamp", "Ormonde").last());
     }
 }
