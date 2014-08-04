c34ab875c22240b49a517ba63632c0645d2c1e84
ken.avery <ken.avery@hp.com>
2014-08-03 22:54:59 -0500

    Added Synchronized List/Collection example
    Change-Id: Ic3d3bf8857dfef0a1e58145a0ceadd2a3c6c535c

---
+++ tutorial/src/main/java/org/ken/avery/collections/implementations/wrappers/SynchronizedCollectionConsumer.java
@@ -0,0 +1,118 @@
+package org.ken.avery.collections.implementations.wrappers;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
+
+public class SynchronizedCollectionConsumer implements Runnable
+{
+    private int job = 0;
+    private Semaphore semaphore = null;
+    private BlockingQueue<String> shutdownQueue = null;
+    private List<Integer> synchronizedList = null;
+    private Collection<Integer> synchronixedCollection = null;
+
+    public SynchronizedCollectionConsumer(
+            final int job,
+            final Semaphore semaphore,
+            final List<Integer> synchronizedList,
+            final Collection<Integer> synchronixedCollection,
+            final BlockingQueue<String> shutdownQueue)
+    {
+        super();
+
+        this.job = job;
+        this.semaphore = semaphore;
+        this.synchronizedList = synchronizedList;
+        this.synchronixedCollection = synchronixedCollection;
+        this.shutdownQueue = shutdownQueue;
+    }
+
+    private void printInitialInformation()
+    {
+        final Random random = new Random(100);
+        try
+        {
+            semaphore.acquire();
+
+            System.out.println(Thread.currentThread().getName() + " Start  : Job = " + job);
+            printCollection("Initial " + job + ":");
+
+            Thread.sleep(random.nextInt(1000));
+        }
+        catch (final InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        semaphore.release();
+    }
+
+    private void printCollection(final String title)
+    {
+        System.out.println(title);
+        int i = 0;
+        for (final Integer value : synchronixedCollection)
+        {
+            if (i++ >= (synchronixedCollection.size() / 2))
+            {
+                i = 0;
+                System.out.println();
+            }
+            System.out.print("[" + value + "]");
+        }
+        System.out.println();
+    }
+
+    private void printFinalInformation()
+    {
+        try
+        {
+            semaphore.acquire();
+
+            printCollection("Final " + job + ":");
+            System.out.println(Thread.currentThread().getName() + " End    : Job = " + job);
+        }
+        catch (final InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        semaphore.release();
+    }
+
+    @Override
+    public void run()
+    {
+        printInitialInformation();
+
+        runJob();
+
+        printFinalInformation();
+
+        shutdown();
+    }
+
+    public void runJob()
+    {
+        for (int i = 0; i < synchronizedList.size(); i++)
+        {
+            synchronized (synchronizedList)
+            {
+                synchronizedList.set(i, (synchronizedList.get(i) + 1));
+            }
+        }
+    }
+
+    private void shutdown()
+    {
+        try
+        {
+            shutdownQueue.put("SHUTDOWN Job " + job);
+        }
+        catch (final InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ tutorial/src/main/java/org/ken/avery/collections/implementations/wrappers/SynchronizedCollectionExample.java
@@ -0,0 +1,125 @@
+package org.ken.avery.collections.implementations.wrappers;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SynchronizedCollectionExample
+{
+    private static final int JOBS = 5;
+    private static final int LIST_ENTRIES = 25;
+    private static final int COLLECTION_ENTRIES = 25;
+    private static Semaphore semaphore = new Semaphore(1);
+    final static BlockingQueue<String> shutdownQueue = new ArrayBlockingQueue<String>(1);
+    static List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<Integer>());
+    final static Collection<Integer> synchronixedCollection = Collections.synchronizedCollection(synchronizedList);
+
+    public static void main(final String[] args)
+    {
+        final ExecutorService executor = Executors.newCachedThreadPool();
+
+        loadJobList();
+        loadJobCollection();
+        printCollection("Initial :");
+        executeConsumerJobs(executor);
+        waitOnJobs();
+        printCollection("Final : ");
+        terminateJobs(executor);
+    }
+
+    private static void printCollection(final String title)
+    {
+        System.out.println(title);
+        int i = 0;
+        synchronized (synchronizedList)
+        {
+            for (final Integer value : synchronizedList)
+            {
+                if (i++ >= LIST_ENTRIES)
+                {
+                    i = 0;
+                    System.out.println();
+                }
+                System.out.print("[" + value + "]");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void loadJobList()
+    {
+        synchronized (synchronizedList)
+        {
+            for (int i = LIST_ENTRIES; i > 0; i--)
+            {
+                synchronizedList.add(100 + i);
+            }
+        }
+    }
+
+    private static void loadJobCollection()
+    {
+        synchronized (synchronixedCollection)
+        {
+            for (int i = COLLECTION_ENTRIES; i > 0; i--)
+            {
+                synchronixedCollection.add(200 + i);
+            }
+        }
+    }
+
+    private static void executeConsumerJobs(final ExecutorService executor)
+    {
+        for (int i = 0; i < JOBS; i++)
+        {
+            executor.execute(new SynchronizedCollectionConsumer(
+                    (i + 1),
+                    semaphore,
+                    synchronizedList,
+                    synchronixedCollection,
+                    shutdownQueue));
+        }
+    }
+
+    private static void waitOnJobs()
+    {
+        int jobsEnded = 0;
+        while (jobsEnded++ < JOBS)
+        {
+            try
+            {
+                final String shutdown = shutdownQueue.take();
+                semaphore.acquire();
+                System.out.println("Received: " + shutdown);
+                semaphore.release();
+            }
+            catch (final InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void terminateJobs(final ExecutorService executor)
+    {
+        try
+        {
+            // waits for termination for 30 seconds only
+            executor.awaitTermination(1, TimeUnit.SECONDS);
+        }
+        catch (final InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        executor.shutdownNow();
+    }
+}
